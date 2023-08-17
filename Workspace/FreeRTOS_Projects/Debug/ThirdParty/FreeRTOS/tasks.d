6bd55805467f53d9240ecc6c0563a10ba97db9a7
Kyungjae Lee <kjxlee@gmail.com>
2023-08-17 12:28:13 -0500

    Create 'Workspace/FreeRTOS_Projects/' - Integrate FreeRTOS into a project for STM32F407 target

---
+++ Workspace/FreeRTOS_Projects/Debug/ThirdParty/FreeRTOS/tasks.d
@@ -0,0 +1,25 @@
+ThirdParty/FreeRTOS/tasks.o: ../ThirdParty/FreeRTOS/tasks.c \
+ /home/klee/repos/freertos-project/Workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/include/FreeRTOS.h \
+ /home/klee/repos/freertos-project/Workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/FreeRTOSConfig.h \
+ /home/klee/repos/freertos-project/Workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/include/projdefs.h \
+ /home/klee/repos/freertos-project/Workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/include/portable.h \
+ /home/klee/repos/freertos-project/Workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/include/deprecated_definitions.h \
+ /home/klee/repos/freertos-project/Workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h \
+ /home/klee/repos/freertos-project/Workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/include/mpu_wrappers.h \
+ /home/klee/repos/freertos-project/Workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/include/task.h \
+ /home/klee/repos/freertos-project/Workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/include/list.h \
+ /home/klee/repos/freertos-project/Workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/include/timers.h \
+ /home/klee/repos/freertos-project/Workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/include/task.h \
+ /home/klee/repos/freertos-project/Workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/include/stack_macros.h
+/home/klee/repos/freertos-project/Workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/include/FreeRTOS.h:
+/home/klee/repos/freertos-project/Workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/FreeRTOSConfig.h:
+/home/klee/repos/freertos-project/Workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/include/projdefs.h:
+/home/klee/repos/freertos-project/Workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/include/portable.h:
+/home/klee/repos/freertos-project/Workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/include/deprecated_definitions.h:
+/home/klee/repos/freertos-project/Workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h:
+/home/klee/repos/freertos-project/Workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/include/mpu_wrappers.h:
+/home/klee/repos/freertos-project/Workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/include/task.h:
+/home/klee/repos/freertos-project/Workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/include/list.h:
+/home/klee/repos/freertos-project/Workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/include/timers.h:
+/home/klee/repos/freertos-project/Workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/include/task.h:
+/home/klee/repos/freertos-project/Workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/include/stack_macros.h:
