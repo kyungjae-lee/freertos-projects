972c9b41863a2aa24b6b1cee06ac3d1cb7a3a42e
Kyungjae Lee <kjxlee@gmail.com>
2023-08-20 10:45:51 -0500

    'Workspace/01_Tasks/' - Create project and integrate FreeRTOS kernel

---
+++ Workspace/01_Tasks/Debug/ThirdParty/FreeRTOS/portable/MemMang/heap_4.d
@@ -0,0 +1,20 @@
+ThirdParty/FreeRTOS/portable/MemMang/heap_4.o: \
+ ../ThirdParty/FreeRTOS/portable/MemMang/heap_4.c \
+ /home/klee/repos/freertos-projects/Workspace/01_Tasks/ThirdParty/FreeRTOS/include/FreeRTOS.h \
+ /home/klee/repos/freertos-projects/Workspace/01_Tasks/ThirdParty/FreeRTOS/FreeRTOSConfig.h \
+ /home/klee/repos/freertos-projects/Workspace/01_Tasks/ThirdParty/FreeRTOS/include/projdefs.h \
+ /home/klee/repos/freertos-projects/Workspace/01_Tasks/ThirdParty/FreeRTOS/include/portable.h \
+ /home/klee/repos/freertos-projects/Workspace/01_Tasks/ThirdParty/FreeRTOS/include/deprecated_definitions.h \
+ /home/klee/repos/freertos-projects/Workspace/01_Tasks/ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h \
+ /home/klee/repos/freertos-projects/Workspace/01_Tasks/ThirdParty/FreeRTOS/include/mpu_wrappers.h \
+ /home/klee/repos/freertos-projects/Workspace/01_Tasks/ThirdParty/FreeRTOS/include/task.h \
+ /home/klee/repos/freertos-projects/Workspace/01_Tasks/ThirdParty/FreeRTOS/include/list.h
+/home/klee/repos/freertos-projects/Workspace/01_Tasks/ThirdParty/FreeRTOS/include/FreeRTOS.h:
+/home/klee/repos/freertos-projects/Workspace/01_Tasks/ThirdParty/FreeRTOS/FreeRTOSConfig.h:
+/home/klee/repos/freertos-projects/Workspace/01_Tasks/ThirdParty/FreeRTOS/include/projdefs.h:
+/home/klee/repos/freertos-projects/Workspace/01_Tasks/ThirdParty/FreeRTOS/include/portable.h:
+/home/klee/repos/freertos-projects/Workspace/01_Tasks/ThirdParty/FreeRTOS/include/deprecated_definitions.h:
+/home/klee/repos/freertos-projects/Workspace/01_Tasks/ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h:
+/home/klee/repos/freertos-projects/Workspace/01_Tasks/ThirdParty/FreeRTOS/include/mpu_wrappers.h:
+/home/klee/repos/freertos-projects/Workspace/01_Tasks/ThirdParty/FreeRTOS/include/task.h:
+/home/klee/repos/freertos-projects/Workspace/01_Tasks/ThirdParty/FreeRTOS/include/list.h:
