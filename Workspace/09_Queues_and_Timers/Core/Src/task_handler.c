ff3590f5c66cbd94f1644f15530d68d5ecc8d5f5
Kyungjae Lee <kjxlee@gmail.com>
2023-09-04 21:46:15 -0500

    Create 'Workspace/09_Queues_and_Timers/'

---
+++ Workspace/09_Queues_and_Timers/Core/Src/task_handler.c
@@ -0,0 +1,42 @@
+#include <main.h>
+
+
+void menu_task_handler(void *param)
+{
+	while (1)
+	{
+
+	}
+}
+
+void cmd_task_handler(void *param)
+{
+	while (1)
+	{
+
+	}
+}
+
+void print_task_handler(void *param)
+{
+	while (1)
+	{
+
+	}
+}
+
+void led_task_handler(void *param)
+{
+	while (1)
+	{
+
+	}
+}
+
+void rtc_task_handler(void *param)
+{
+	while (1)
+	{
+
+	}
+}
